90e77ec110b742440ef87584c0490946d78ff106
eettoree <studiodesigner95@gmail.com>
2025-02-26 22:29:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// Rimuovere il pannello di controllo GUI
+function startGUI () {
+    return; // Disabilita completamente il pannello GUI
+}
+
+// Mobile promo section - Disabilitata
+const promoPopup = document.getElementsByClassName('promo')[0];
+if (promoPopup) promoPopup.style.display = 'none';
+
+// Simulation section
+const canvas = document.getElementsByTagName('canvas')[0];
+resizeCanvas();
+
+let config = {
+    SIM_RESOLUTION: 128,
+    DYE_RESOLUTION: 1024,
+    CAPTURE_RESOLUTION: 512,
+    DENSITY_DISSIPATION: 1,
+    VELOCITY_DISSIPATION: 0.2,
+    PRESSURE: 0.55, 
+    PRESSURE_ITERATIONS: 20,
+    CURL: 15, 
+    SPLAT_RADIUS: 0.25,
+    SPLAT_FORCE: 6000,
+    SHADING: true,
+    COLORFUL: false, 
+    COLOR_UPDATE_SPEED: 10,
+    PAUSED: false,
+    BACK_COLOR: { r: 0, g: 0, b: 0 },
+    TRANSPARENT: false,
+    BLOOM: true,
+    BLOOM_ITERATIONS: 8,
+    BLOOM_RESOLUTION: 256,
+    BLOOM_INTENSITY: 0.8,
+    BLOOM_THRESHOLD: 0.6,
+    BLOOM_SOFT_KNEE: 0.7,
+    SUNRAYS: true,
+    SUNRAYS_RESOLUTION: 196,
+    SUNRAYS_WEIGHT: 1.0,
+}
+
+const { gl, ext } = getWebGLContext(canvas);
+
+if (isMobile()) {
+    config.DYE_RESOLUTION = 512;
+}
+if (!ext.supportLinearFiltering) {
+    config.DYE_RESOLUTION = 512;
+    config.SHADING = false;
+    config.BLOOM = false;
+    config.SUNRAYS = false;
+}
+
+updateKeywords();
+initFramebuffers();
+multipleSplats(parseInt(Math.random() * 20) + 5);
+
+let lastUpdateTime = Date.now();
+let colorUpdateTimer = 0.0;
+update();
+
+function update () {
+    const dt = calcDeltaTime();
+    if (resizeCanvas())
+        initFramebuffers();
+    updateColors(dt);
+    applyInputs();
+    if (!config.PAUSED)
+        step(dt);
+    render(null);
+    requestAnimationFrame(update);
+}
